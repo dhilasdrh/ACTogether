c4f9867bbc3a5efce83c8763280912b05d2b7fb1
dhilasdrh <nur.fadhilah19@gmail.com>
2021-04-12 07:02:34 +0800

    get user data from firebaseauth

--- app/src/main/java/com/dhilasdrh/zerowaste/activity/ProfileActivity.kt
+++ app/src/main/java/com/dhilasdrh/zerowaste/activity/ProfileActivity.kt
@@ -1,17 +1,19 @@
 package com.dhilasdrh.zerowaste.activity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LifecycleRegistry
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.dhilasdrh.zerowaste.MyApplication
 import com.dhilasdrh.zerowaste.R
 import com.dhilasdrh.zerowaste.databinding.ActivityProfileBinding
-import com.dhilasdrh.zerowaste.databinding.FragmentProfileBinding
 import com.dhilasdrh.zerowaste.ui.profile.ProfileViewModel
 import com.dhilasdrh.zerowaste.viewmodel.AuthViewModel
+import com.firebase.ui.auth.AuthUI
 
 class ProfileActivity : AppCompatActivity() {
     private lateinit var authViewModel: AuthViewModel
@@ -27,10 +29,38 @@ class ProfileActivity : AppCompatActivity() {
         profileViewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
         authViewModel = ViewModelProvider(this).get(AuthViewModel::class.java)
 
-        getUserInfo()
+        //getUserInfo()
+        getUserData()
 
         supportActionBar?.title = "Account"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun getUserData(){
+        profileViewModel.displayName.observe(this, {
+            binding.tvFullname.text = it
+        })
+        profileViewModel.email.observe(this, {
+            binding.tvEmail.text = it
+        })
+        profileViewModel.photoUrl.observe(this, {
+            if (it != null) {
+                Glide.with(this).load(it).into(binding.imgProfile)
+            } else {
+                Glide.with(this).load(R.mipmap.ic_launcher_round).into(binding.imgProfile)
+            }
+        })
+
+        //sign-out
+        binding.btnSignOut.setOnClickListener {
+            this.let { it1 ->
+                AuthUI.getInstance()
+                    .signOut(it1)
+                    .addOnCompleteListener {
+                        startActivity(Intent(this, LoginActivity::class.java))
+                    }
+            }
+        }
     }
 
     private fun getUserInfo() {
